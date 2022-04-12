fc7ce89a0b0d4a6eccc3ebdc6f79603ed77ecf00
Alessandro Pietrantonio <alessandro.pietrantonio@u2y.it>
2022-04-13 00:07:52 +0200

    add model

---
+++ src/Models/HubspotToken.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace U2y\Hubspot\Models\Hubspot;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HubspotToken extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'access_token',
+        'refresh_token',
+        'expire_at',
+    ];
+
+    protected $casts = [
+        'expire_at' => 'datetime'
+    ];
+}
