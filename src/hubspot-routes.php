9a480120e407328a17dc092a851b3962ed4f386c
Alessandro Pietrantonio <alessandro.pietrantonio@u2y.it>
2022-04-12 23:34:04 +0200

    fix controller use

--- src/hubspot-routes.php
+++ src/hubspot-routes.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+use U2y\Hubspot\Http\Controllers\HubspotController;
 
 app('router')->prefix('hubspot')->middleware('auth:admin')->group(function() {
     app('router')->get('', [HubspotController::class, 'index'])->name('hubspot.auth');
