d22e5a62955546ab7289ea8c8baf8e16f121a920
Alessandro Pietrantonio <alessandro.pietrantonio@u2y.it>
2022-04-12 23:37:28 +0200

    fix middleware route

--- config/hubspot.php
+++ config/hubspot.php
@@ -1,6 +1,7 @@
 <?php 
 
 return [
+    'auth_middleware' => env('HUBSPOT_AUTH_MIDDLEWARE', 'auth:web'),
     'app_id' => env('HUBSPOT_APP_ID'),
     'client_id' => env('HUBSPOT_CLIENT_ID'),
     'client_secret' => env('HUBSPOT_CLIENT_SECRET')
--- src/hubspot-routes.php
+++ src/hubspot-routes.php
@@ -2,7 +2,7 @@
 
 use U2y\Hubspot\Http\Controllers\HubspotController;
 
-app('router')->prefix('hubspot')->middleware('auth:admin')->group(function() {
+app('router')->prefix('hubspot')->middleware(config('hubspot.auth_middleware'))->group(function() {
     app('router')->get('', [HubspotController::class, 'index'])->name('hubspot.auth');
-    app('router')->get('auth-callback', [HubspotController::class, 'callback'])->name('hubspot.auth_callback')->withoutMiddleware('auth:admin');
+    app('router')->get('auth-callback', [HubspotController::class, 'callback'])->name('hubspot.auth_callback')->withoutMiddleware(config('hubspot.auth_middleware'));
 });
