fcc9ab6059c20923d2b34c19ecf51eb132f64bd6
Alessandro Pietrantonio <alessandro.pietrantonio@u2y.it>
2022-07-29 11:47:15 +0200

    fix deals search by stages using http laravel client

--- src/Services/Resources/Deals.php
+++ src/Services/Resources/Deals.php
@@ -1,15 +1,19 @@
-<?php 
+<?php
 
 namespace U2y\Hubspot\Services\Resources;
 
+use GuzzleHttp\Client;
+use Http;
 use U2y\Hubspot\Services\Traits\Filter;
 use HubSpot\Client\Crm\Contacts\Model\PublicObjectSearchRequest;
+use U2y\Hubspot\Models\HubspotToken;
+use U2y\Hubspot\Services\HubspotService;
 use U2y\Hubspot\Services\Traits\FormatResponse;
 
 class Deals
 {
     use Filter, FormatResponse;
-    
+
     public $client;
 
     public function __construct($client)
@@ -34,17 +38,48 @@ class Deals
         return $this->formatResponse($this->list());
     }
 
+    // Questa dovrebbe essere la chiamata corretta con il client ufficiale di HS, ma pare non funzionare
+    // public function listByStages(array $stages)
+    // {
+    //     $filterGroups = $this->filterByStages($stages);
+    //     $searchRequest = new PublicObjectSearchRequest();
+    //     $searchRequest->setFilterGroups([...$filterGroups]);
+    //     $searchRequest->setLimit(100);
+    //     $searchRequest->setSorts("ASCENDING");
+    //     $searchRequest->setProperties(["hs_object_id"]);
+    //     $searchRequest->setAfter(0);
+    //     $deals = $this->client->crm()->deals()->searchApi()->doSearch($searchRequest);
+    //     if (empty($deals->getResults())) {
+    //         return null;
+    //     }
+    //     return $deals->getResults();
+    // }
+
+    // Usiamo questo metodo con un client ad hoc ed evitiamo di usare quello ufficiale di HS
+    // perché sembra esserci un problema con il search sulle deals
     public function listByStages(array $stages)
     {
-        $filterGroups = $this->filterByStages($stages);
-        $searchRequest = $this->createSearchRequest($filterGroups);
-        $dealsPage = $this->client->crm()->deals()->searchApi()->doSearch($searchRequest);
+        // Forzo il refresh eventuale del token
+        new HubspotService();
+        // Prendo l'ultimo token dal db
+        $last_token = HubspotToken::orderBy('expire_at', 'desc')->first();
+        if (!$last_token) {
+            throw new \Exception('Not Hubspot token found. Please generate one');
+        }
 
-        if (empty($dealsPage->getResults())) {
+        $result = Http::withToken($last_token->access_token)->post('https://api.hubapi.com/crm/v3/objects/deals/search', [ 
+                'filterGroups' => $this->filterByStages($stages),
+                'limit' => 100, 
+                'after' => 0
+            ]);
+                
+        $deals = $result->json()['results'];
+
+        if (empty($deals)) {
             return null;
         }
 
-        return $dealsPage->getResults();
+        return $deals;
     }
 
     public function formattedListByStages(array $stages)
